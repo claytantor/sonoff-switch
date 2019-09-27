ba9a9d0dceb278fb9eced85483f6ac079e974c44
claytantor <claytantor@gmail.com>
2019-09-26 21:08:38 -0700

    added configuration

--- README.md
+++ README.md
@@ -7,22 +7,18 @@ python3 -m venv ./venv
 source venv/bin/activate
 $(pwd)/venv/bin/python3 -m pip install --upgrade pip
 $(pwd)/venv/bin/python3 -m pip install -r requirements.txt
-
-
 ```
-
-python3 -m pip install -r requirements.txt
 
 # creating the systemd service
 ```
-$(pwd)/venv/bin/python3 makeservice.py -d $(pwd) -t sonoff-switch.service.mustache > sonoff-switch.service
+$(pwd)/venv/bin/python3 makeservice.py -d $(pwd) -t sonoff.service.mustache > sonoff.service
 ```
 
 Instructions for setting up your service can be found at https://www.raspberrypi-spy.co.uk/2015/10/how-to-autorun-a-python-script-on-boot-using-systemd/
 
 ```
-sudo cp sonoff-switch.service /lib/systemd/system/sonoff-switch.service
-sudo chmod 644 /lib/systemd/system/sonoff-switch.service
+sudo cp sonoff.service /lib/systemd/system/sonoff.service
+sudo chmod 644 /lib/systemd/system/sonoff.service
 sudo systemctl daemon-reload
-sudo systemctl enable sonoff-switch.service
+sudo systemctl enable sonoff.service
 ```
--- app.py
+++ app.py
@@ -1,39 +1,45 @@
 import schedule
 import requests
-import sys
+import sys, os
 import time
-
-devices = {
-    'living_room':'192.168.5.59'
-}
-
+import argparse
+import yaml
 
 # def job():
 #     print("I'm working...")
 #     r = requests.get('http://192.168.5.59/cm?cmnd=Power%20TOGGLE')
 #     # /cm?cmnd=Power%20TOGGLE
 #     # /cm?cmnd=Power%20off'
+#     # /cm?cmnd=Power%20on'
 
-def sonoff_on(ip_addr):
-    print("turning on {0}".format(ip_addr))
-    r = requests.get('http://{0}/cm?cmnd=Power%20on'.format(ip_addr))
-
-def sonoff_off(ip_addr):
-    print("turning off {0}".format(ip_addr))
-    r = requests.get('http://{0}/cm?cmnd=Power%20off'.format(ip_addr))
-
-
-def sonoff_on_living_room():
-    sonoff_on(devices['living_room'])
-def sonoff_off_living_room():
-    sonoff_off(devices['living_room'])
+def sonoff_job(ip_addr, sonoff_command):
+    print("setting {0} to {1}".format(ip_addr, sonoff_command))
+    r = requests.get('http://{0}/cm?cmnd={1}'.format(ip_addr, sonoff_command))
     
-
+def loadConfig(configFile):
+    cfg = None
+    with open(configFile, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    return cfg
 
 def main(argv):
+    print("starting sonoff app.")
+
+    # Read in command-line parameters
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--config", action="store", 
+        required=True, dest="config", help="the YAML configuration file")
+
+    args = parser.parse_args()
+    config = loadConfig(args.config)
+
     print("starting sonoff scheduler app.")
-    schedule.every().day.at("19:15").do(sonoff_on_living_room)
-    schedule.every().day.at("23:30").do(sonoff_off_living_room)
+    lr = config['devices']['living-room']
+    for schedule_item in lr['schedule']:
+        print(schedule_item)
+        schedule.every().day.at(schedule_item['time']).do(sonoff_job, 
+            ip_addr=config['devices']['living-room']['ip'], sonoff_command=schedule_item['command'])
 
     while True:
         schedule.run_pending()
